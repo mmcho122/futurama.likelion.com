32e8460e121427d3a9cc95fc2f203bf5f97d1d69
mijin <mmcho122@gmail.com>
2021-12-26 15:42:16 +0900

    ✨ futurama 페이지 생성

---
+++ src/pages/futurama.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const futuramaPage = () => {
+    return (
+      <div>
+          Futurama
+      </div>
+    )
+  }
+  
+  export default futuramaPage
+  
